d083ee303d350237c0df023bbfc791755e9dc1e3
shani11 <shanibhati17@gmail.com>
2019-06-05 17:47:53 +0530

    First commit

---
+++ LogFourNet.cs
@@ -0,0 +1,44 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+
+public static class LogFourNet
+{
+    // Define a static logger variable so that it references the
+    private static readonly ILog Log = LogManager.GetLogger(typeof(LogFourNet));
+
+    static LogFourNet()
+     {
+        var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+        XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        Log.Info("Log4net is configured.");
+    }
+
+    public static void Info(object currentObj, string msg, [CallerLineNumber] int lineNumber = 0,
+        [CallerFilePath] string caller = "", [CallerMemberName] string memberName = "")
+    {
+        Log.Info("[" + currentObj.GetType().Namespace + "." +
+                 Path.GetFileNameWithoutExtension(caller) + "." + memberName + ":" + lineNumber + "] - " + msg);
+    }
+
+    public static void Debug(object currentObj, string msg, [CallerLineNumber] int lineNumber = 0,
+        [CallerFilePath] string caller = "", [CallerMemberName] string memberName = "")
+    {
+        Log.Debug("[" + currentObj.GetType().Namespace + "." +
+                  Path.GetFileNameWithoutExtension(caller) + "." + memberName + ":" + lineNumber + "] - " + msg);
+    }
+
+    public static void Error(object currentObj, string msg, [CallerLineNumber] int lineNumber = 0,
+        [CallerFilePath] string caller = "", [CallerMemberName] string memberName = "")
+    {
+        Log.Error("[" + currentObj.GetType().Namespace + "." +
+                  Path.GetFileNameWithoutExtension(caller) + "." + memberName + ":" + lineNumber + "] - " + msg);
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Log4net_Core2._2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+         //   //To Stop Microsoft Loggings
+         //   services.AddLogging(
+         //builder =>
+         //{
+         //    builder.AddFilter("Microsoft", LogLevel.Warning)
+         //           .AddFilter("System", LogLevel.Warning)
+         //           .AddFilter("NToastNotify", LogLevel.Warning)
+         //           .AddConsole();
+         //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            //loggerFactory.AddLog4Net(Configuration.GetValue<string>("Log4NetConfigFile:Name"));
+
+
+
+
+            app.UseMvc();
+        }
+    }
+}
